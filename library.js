622845164cb0e8c28c7172ac5d1ccfbac293bd48
Ian Wulf <ianwulf24@gmail.com>
2025-09-24 16:57:24 -0500

    Basic Setup -Have title, a non-functioning 'new book' button, and three book 'cards' initially displayed with relevant info for each (All generated on page load) -Using grid layout system for page and cards -Cards formatted for readability, and left space for buttons on each card.

--- library.js
+++ library.js
@@ -1,6 +1,8 @@
 // Stores books
 const myLibrary = [];
 
+let body = document.querySelector("body");
+
 // Constructs each book
 function Book(title, author, pubDate, read) {
     let id = crypto.randomUUID(); // unique identifier
@@ -11,7 +13,7 @@ function Book(title, author, pubDate, read) {
 }
 
 // Toggle 'read' status of a book
-Book.prototype.isRead = function() {
+Book.prototype.toggleRead = function() {
     if(this.read = "read") {
         this.read = "not read";
     } else {
@@ -25,11 +27,52 @@ function addBook(title, author, pubDate, read) {
     myLibrary.push(newBook);
 }
 
-// Display each book
 function displayBooks() {
-    // Testing in console for now
+    // Container for book display
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    body.appendChild(grid);
+    // Add books
     myLibrary.forEach(function(book) {
-        console.log(book)
+        // console.log(book)
+        let card = document.createElement("div");
+        card.classList.add("bookCard");
+        for(const key in book) {
+            if(book.hasOwnProperty(key)) {
+                let info = document.createElement("div");
+                let property = document.createElement("p");
+                let value = document.createElement("p");
+
+                property.innerHTML = `${key.toUpperCase()}:`;
+                value.innerHTML = `${book[key]}`; 
+                switch (key) {
+                    case "title":
+                        property.setAttribute("id","title");
+                        value.setAttribute("id","title-value");
+                        break;
+                    case "author":
+                        property.setAttribute("id","author");
+                        value.setAttribute("id","author-value");
+                        break;
+                    case "pubDate":
+                        property.setAttribute("id","pubDate");
+                        value.setAttribute("id","pubDate-value");
+                        break;
+                    case "read":
+                        property.setAttribute("id","read");
+                        value.setAttribute("id","read-value");
+                        value.innerHTML = value.innerHTML.toUpperCase();
+                        break;
+                }
+               
+                info.appendChild(property);
+                info.appendChild(value);
+                card.appendChild(info);
+            }
+        }
+
+        grid.appendChild(card);
+
     });
 }
 
@@ -37,4 +80,5 @@ function displayBooks() {
 addBook('Lord of the Rings: The Fellowship of the Ring', 'J.R.R. Tolkien', '07-29-1954', 'read');
 addBook('Halo: First Strike','Eric Nylund','12-02-2003','read');
 addBook("Ender's Game",'Orson Scott Card','01-15-1985','read');
+
 displayBooks();
